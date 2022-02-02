e08c09ed1182038003ce2fb9a63b0a7e6898e82c
Taha <93734684+Taha-Haghgoo@users.noreply.github.com>
2022-02-02 09:29:08 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+//use ytdl-core to download music video on youtube and stream it
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
@@ -63,6 +64,7 @@ if (message.startsWith('!play')) {
     channel_id: `${context.params.event.channel_id}`,
     content: `Tuna paused the song.`,
   });
+  //resume command function
 } else if (message.startsWith('!resume')) {
     await lib.discord.voice['@0.0.1'].tracks.resume({
     guild_id: `${context.params.event.guild_id}`
@@ -71,78 +73,3 @@ if (message.startsWith('!play')) {
     channel_id: `${context.params.event.channel_id}`,
     content: `Tuna resumed the song.`,
   });
-=======
-const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
-const ytdl = require('ytdl-core');
-const ytSearch = require('yt-search');
-
-let VOICE_CHANNEL = '937076437112979526';
-let message = context.params.event.content;
-// play function
-if (message.startsWith('!play')) {
-  let searchString = message.split(' ').slice(1).join(' ');
-  //serach in youtube 
-  try {
-    let youtubeLink;
-    if (!searchString) {
-        return lib.discord.channels['@0.2.0'].messages.create({
-        channel_id: `${context.params.event.channel_id}`,
-        content: `No search string provided!`,
-      });
-    }
-    if (!searchString.includes('youtube.com')) {
-      let results = await ytSearch(searchString);
-        if (!results?.all?.length) {
-          return lib.discord.channels['@0.2.0'].messages.create({
-          channel_id: `${context.params.event.channel_id}`,
-          content: `No results found for your search string. Please try a different one.`,
-        });
-      }
-      youtubeLink = results.all[0].url;
-    } else {
-      youtubeLink = searchString;
-    }
-    let downloadInfo = await ytdl.getInfo(youtubeLink);
-    await lib.discord.voice['@0.0.1'].tracks.play({
-      channel_id: `${VOICE_CHANNEL}`,
-      guild_id: `${context.params.event.guild_id}`,
-      download_info: downloadInfo
-    });
-    return lib.discord.channels['@0.2.0'].messages.create({
-      channel_id: `${context.params.event.channel_id}`,
-      content: `Tuna Now playing **${downloadInfo.videoDetails.title}**`,
-    });
-  } catch (e) {
-    return lib.discord.channels['@0.2.0'].messages.create({
-      channel_id: `${context.params.event.channel_id}`,
-      content: `Failed to play track!`,
-    });
-    }
-    // stop command function
-} else if (message.startsWith('!leave')) {
-    await lib.discord.voice['@0.0.1'].channels.disconnect({
-    guild_id: `${context.params.event.guild_id}`
-  });
-    await lib.discord.channels['@0.2.0'].messages.create({
-    channel_id: `${context.params.event.channel_id}`,
-    content: `Tuna Disconnected from <#${VOICE_CHANNEL}>!`,
-  });
-  //pause command function
-} else if (message.startsWith('!pause')) {
-    await lib.discord.voice['@0.0.1'].tracks.pause({
-    guild_id: `${context.params.event.guild_id}`
-  });
-    return lib.discord.channels['@0.2.0'].messages.create({
-    channel_id: `${context.params.event.channel_id}`,
-    content: `Tuna paused the song.`,
-  });
-} else if (message.startsWith('!resume')) {
-    await lib.discord.voice['@0.0.1'].tracks.resume({
-    guild_id: `${context.params.event.guild_id}`
-  });
-    return lib.discord.channels['@0.2.0'].messages.create({
-    channel_id: `${context.params.event.channel_id}`,
-    content: `Tuna resumed the song.`,
-  });
->>>>>>> 5dba225021f87462947a656513a6c955471f8953
-}
