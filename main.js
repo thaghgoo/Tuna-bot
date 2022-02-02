7254d416caa18c9f2eb97d8c0f5c24259b1a4eb7
Taha <93734684+Taha-Haghgoo@users.noreply.github.com>
2022-02-02 09:38:58 +0330

    update comments

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+//Hello there welcome to tuna bot source code
 //use ytdl-core to download music video on youtube and stream it
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 const ytdl = require('ytdl-core');
